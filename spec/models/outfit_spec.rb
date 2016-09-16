d2c38be8eb237211674638ffcd219b1f3efce5d7
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-09-16 12:38:55 -0400

    rubocop style fixes

--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -1,15 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Outfit, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  
-  # describe 'associations' do
-  #   it { should belong_to(:user) }
-  # end
-
   describe 'validations' do
     it { should validate_presence_of(:rating) }
-
   end
-
 end
