29b245e549bd394248febe8e2acc41d1690e40f0
sunshead <sunshead@gmail.com>
2016-09-23 10:29:19 -0400

    delete unnecessary test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 describe User do
 
-=begin
-  describe 'Associations' do
-    it { should have_many(:outfits) }
-  end
-=end
-
   describe 'Validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:uid) }
@@ -20,7 +14,6 @@ describe User do
     it { should validate_attachment_content_type(:avatar).
                   allowing('image/png', 'image/gif').
                   rejecting('text/plain', 'text/xml') }
-    #it { should validate_attachment_size(:avatar).
-                #less_than(2.megabytes) }
+    binding.pry
   end
 end
