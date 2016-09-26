566b7111a3fe101f35f632252604c8ff77136a20
sunshead <sunshead@gmail.com>
2016-09-26 10:59:57 -0400

    add avatar to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < BaseSerializer
-  attributes :name, :email, :gender, :weather_perception, :preferred_time
+  attributes :name, :email, :gender, :weather_perception, :preferred_time,
+             :avatar_file_name
 end
