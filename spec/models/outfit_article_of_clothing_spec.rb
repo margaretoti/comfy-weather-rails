6d85c3d997f46b4528824f56afd121f31b06d767
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-09-28 12:25:36 -0400

    tested presence_of validations for OutfitArticleOfClothing model

--- spec/models/outfit_article_of_clothing_spec.rb
+++ spec/models/outfit_article_of_clothing_spec.rb
@@ -5,4 +5,9 @@ describe OutfitArticleOfClothing do
     it { should belong_to(:outfit) }
     it { should belong_to(:article_of_clothing) }
   end
+
+  describe 'validationss' do
+    it { should validate_presence_of(:outfit) }
+    it { should validate_presence_of(:article_of_clothing) }
+  end
 end
