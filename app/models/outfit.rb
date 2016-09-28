e45794f1435929753ced9542ae130aa15f2a6396
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-09-28 13:40:38 -0400

    added has many through relationship to Outfit and ArticleOfClothing models

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,6 +1,9 @@
 class Outfit < ActiveRecord::Base
   belongs_to :user
 
+  has_many :outfit_article_of_clothings
+  has_many :article_of_clothings, through: :outfit_article_of_clothings
+
   has_attached_file :photo
 
   validates_attachment :photo, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'] }
