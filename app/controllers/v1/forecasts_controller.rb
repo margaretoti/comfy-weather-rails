4a2360b7d8559c86083bc9951e30757f97ce2d44
sunshead <sunshead@gmail.com>
2016-09-29 23:11:57 -0400

    remove forecast serializer

--- app/controllers/v1/forecasts_controller.rb
+++ app/controllers/v1/forecasts_controller.rb
@@ -2,6 +2,6 @@ class V1::ForecastsController < ApplicationController
   def create
     forecast = WeatherForecast.get_weather(latitude: params[:latitude],
     longitude: params[:longitude], period: params[:period])
-    render json: forecast, serializer: ForecastSerializer
+    render json: {forecast: forecast}
   end
 end
--- app/serializers/forecast_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ForecastSerializer < BaseSerializer
-  attributes :summary, :icon, :precipProbability, :temperature,
-             :apparentTemperature, :humidity, :windSpeed, :cloudCover, :pressure
-end
