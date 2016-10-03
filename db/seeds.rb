4f927833b81b91f61a5d4ca5ed87ee4d9636ae1b
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-10-03 10:42:40 -0400

    seeded database with categories and corresponding icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,43 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-category_data = [ [name: 'Blouse', icon: 'blouse'],
-                  [name: 'Button-down', icon: 'button_down'],
-                  [name: 'Flannel', icon: 'flannel'],
-                  [name: 'Polo', icon: 'polo'],
-                  [name: 'Long Sleeve', icon: 'long_sleeve'],
-                  [name: 'Tank', icon: 'tank'],
-                  [name: 'T-Shirt', icon: 't-shirt'],
-                  [name: 'Vest', icon: 'vest'],
-                  [name: 'Sweater', icon: 'sweater'],
-                  [name: 'Sweatshirt', icon: 'sweatshirt'],
-                  [name: 'Jacket', icon: 'jacket'],
-                  [name: 'Coat', icon: 'coat'],
-                  [name: 'Jeans', icon: 'jeans'],
-                  [name: 'Sweatpants', icon: 'sweatpants' ],
-                  [name: 'Leggings', icon: 'leggings'],
-                  [name: 'Skirt', icon: 'skirt'],
-                  [name: 'Dress', icon: 'dress'],
-                  [name: 'Shorts', icon: 'shorts'],
-                  [name: 'Heels', icon: 'heels'],
-                  [name: 'Sandals', icon: 'sandals'],
-                  [name: 'Slippers', icon: 'slippers'],
-                  [name: 'Flats', icon: 'flats'],
-                  [name: 'Boots', icon: 'rain_boots'],
-                  [name: 'Sneaks', icon: 'sneaks'],
-                  [name: 'Dress Shoes', icon: 'dress_shoes'],
-                  [name: 'Loafers'], icon: 'loafers'],
-                  [name: 'Hat', icon: 'hat'],
-                  [name: 'Scarf', icon: 'scarf'],
-                  [name: 'Cap', icon: 'cap'],
-                  [name: 'Mittens', icon: 'mittens'],
-                  [name: 'Other', icon: 'other'],
-                ]
+category_data = [ { name: 'Blouse', icon: 'blouse' },
+                  { name: 'Button-down', icon: 'button_down' },
+                  { name: 'Polo', icon: 'polo' },
+                  { name: 'Long Sleeve', icon: 'long_sleeve' },
+                  { name: 'Tank', icon: 'tank' },
+                  { name: 'T-Shirt', icon: 't_shirt' },
+                  { name: 'Vest', icon: 'vest' },
+                  { name: 'Sweater', icon: 'sweater' },
+                  { name: 'Sweatshirt', icon: 'sweatshirt' },
+                  { name: 'Jacket', icon: 'jacket' },
+                  { name: 'Coat', icon: 'coat' },
+                  { name: 'Jeans', icon: 'jeans' },
+                  { name: 'Sweatpants', icon: 'sweatpants' },
+                  { name: 'Leggings', icon: 'leggings' },
+                  { name: 'Skirt', icon: 'skirt' },
+                  { name: 'Dress', icon: 'dress' },
+                  { name: 'Shorts', icon: 'shorts' },
+                  { name: 'Heels', icon: 'heels' },
+                  { name: 'Sandals', icon: 'sandals' },
+                  { name: 'Slippers', icon: 'slippers' },
+                  { name: 'Flats', icon: 'flats' },
+                  { name: 'Boots', icon: 'boots' },
+                  { name: 'Rain boots', icon: 'rain_boots' },
+                  { name: 'Sneaks', icon: 'sneaks' },
+                  { name: 'Dress Shoes', icon: 'dress_shoes' },
+                  { name: 'Loafers', icon: 'loafers' },
+                  { name: 'Hat', icon: 'hat' },
+                  { name: 'Scarf', icon: 'scarf' },
+                  { name: 'Cap', icon: 'cap' },
+                  { name: 'Mittens', icon: 'mittens' },
+                  { name: 'Other', icon: 'other'} ]
 
-category_data.each do |category_name|
-  Category.create!(name: name, icon: fixture_file_upload(Rails.root.join('lib', 'assets', 'selected_icons', "#{icon}+'.svg'"), 'image/svg'),
-                                                         icon: fixture_file_upload(Rails.root.join('lib', 'assets', 'unselected_icons', "#{icon}+'.svg'"), 'image/svg') )
+category_data.each do |category|
+  puts "Seeding category: #{category[:name]}"
+  article_category = Category.find_or_create_by(name: category[:name])
+  article_category.update!(name: category[:name],
+                            selected_icon: fixture_file_upload(Rails.root.join('lib', 'assets', 'selected_icons', "#{category[:icon] + '.svg'}"), 'image/svg'),
+                            unselected_icon: fixture_file_upload(Rails.root.join('lib', 'assets', 'unselected_icons', "#{category[:icon] + '.svg'}"), 'image/svg')
+                          )
 end
