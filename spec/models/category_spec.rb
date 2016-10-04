d31d2beffb07e2ff792120786d48e839aff9d6a0
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-10-04 14:13:43 -0400

    added icon validation tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,4 +10,15 @@ describe Category do
     it { should validate_presence_of(:selected_icon) }
     it { should validate_presence_of(:unselected_icon) }
   end
+
+  describe 'icons' do
+    it { should have_attached_file(:selected_icon) }
+    it { should validate_attachment_presence(:selected_icon) }
+    it { should validate_attachment_content_type(:selected_icon).
+                  allowing('image/png', 'image/svg', 'image/svg+xml') }
+    it { should have_attached_file(:unselected_icon) }
+    it { should validate_attachment_presence(:unselected_icon) }
+    it { should validate_attachment_content_type(:unselected_icon).
+                  allowing('image/png', 'image/svg', 'image/svg+xml') }
+  end
 end
