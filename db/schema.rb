c0c08faff99b07c35f4320dae3a0287dedc54972
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-10-05 11:19:42 -0400

    resolved additonal schema conflicts, outfit tests working

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20161004191120) do
-=======
-ActiveRecord::Schema.define(version: 20161003200951) do
->>>>>>> b0e4e4a... added location to outfit migratation and model, model tests are working
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Outfit do
   describe 'validations' do
-    it { should validate_presence_of(:user)
+    it { should validate_presence_of(:user) }
     it { should validate_presence_of(:latitude) }
     it { should validate_presence_of(:longitude) }
     it { should validate_length_of(:notes).is_at_most(250) }
