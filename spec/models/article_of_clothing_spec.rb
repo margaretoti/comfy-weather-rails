ee052207baf110ec92404f6c494ab64c75505959
margaretoti <maggie@Maggies-MacBook-Pro.local>
2016-10-07 10:59:58 -0400

    added authorization headers to outfit and article of clothing

--- spec/models/article_of_clothing_spec.rb
+++ spec/models/article_of_clothing_spec.rb
@@ -13,4 +13,14 @@ describe ArticleOfClothing do
     it { should have_many(:outfit_article_of_clothings) }
     it { should have_many(:outfits).through(:outfit_article_of_clothings) }
   end
+
+  describe 'model methods' do
+    describe 'ArticleOfClothing frequency' do
+      it 'returns the number of outfits that a specific article of clothing belongs to' do
+        article = create(:article_of_clothing)
+        outfit_article = create_list(:outfit_article_of_clothing, 3, article_of_clothing: article)
+
+        expect(article.frequency).to eq(3)
+      end
+    end
 end
--- spec/requests/v1/article_of_clothings_requests_spec.rb
+++ spec/requests/v1/article_of_clothings_requests_spec.rb
@@ -1,22 +1,11 @@
 require 'rails_helper'
 
-describe 'ArticleOfClothing frequency' do
-  it 'returns the number of outfits that a specific article of clothing belongs to' do
-    article = create(:article_of_clothing)
-    outfit_article = create_list(:outfit_article_of_clothing, 3, article_of_clothing: article)
-
-    expect(article.frequency).to eq(3)
-  end
-end
-
 describe 'ArticlesOfClothing endpoints' do
   describe 'GET /article_of_clothings' do
     it 'returns the JSON for the existing articles of clothing' do
-
-      articles = create_list(:article_of_clothing, 3)
-
-      get(article_of_clothings_url, {}, accept_headers)
-
+      user = create(:user)
+      articles = create_list(:article_of_clothing, 3, user: user)
+      get(article_of_clothings_url, {}, authorization_headers(user))
       expect(response).to have_http_status :ok
       expect(response.body).to have_json_size(3).at_path('article_of_clothings')
     end
@@ -35,7 +24,6 @@ describe 'ArticlesOfClothing endpoints' do
       }
 
       post(article_of_clothings_url, article_of_clothing_params.to_json, authorization_headers(user))
-
       expect(response).to have_http_status :ok
       expect(response.body).to have_json_path('article_of_clothing/id')
       expect(response.body).to have_json_path('article_of_clothing/created_at')
