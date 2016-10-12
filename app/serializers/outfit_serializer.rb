01379a3d28ce3751f03c5fe3e8c2eddc1975cee3
sunshead <sunshead@gmail.com>
2016-10-12 17:49:32 -0400

    add env variable for afternoon_hour, modify resource restrictions in route

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,8 +1,11 @@
 class OutfitSerializer < BaseSerializer
+<<<<<<< HEAD
   AFTERNOON_HOUR = 15
   
   has_many :article_of_clothings
 
+=======
+>>>>>>> b29489a... add env variable for afternoon_hour, modify resource restrictions in route
   attributes :latitude, :longitude, :notes, :is_public, :photo_url,
              :outfit_weather_types, :weather_types, :weather
 
@@ -13,6 +16,6 @@ class OutfitSerializer < BaseSerializer
   def weather
     ForecastIO
       .forecast(object.latitude, object.longitude, time: object.created_at.to_i)
-      .hourly.data[AFTERNOON_HOUR]
+      .hourly.data[env['AFTERNOON_HOUR']]
   end
 end
