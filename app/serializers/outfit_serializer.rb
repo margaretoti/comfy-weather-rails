b296209824f172e6ed4a55b3bea37d7cdf644a3d
sunshead <sunshead@gmail.com>
2016-10-12 15:43:51 -0400

    fetch weather date for previous outfit

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,8 +1,15 @@
 class OutfitSerializer < BaseSerializer
+  AFTERNOON_HOUR = 15
   attributes :latitude, :longitude, :notes, :is_public, :photo_url,
-             :outfit_weather_types, :weather_types
+             :outfit_weather_types, :weather_types, :weather
 
   def photo_url
     object.photo.url
+  end
+
+  def weather
+    ForecastIO
+      .forecast(object.latitude, object.longitude, time: object.updated_at.to_i)
+      .hourly.data[AFTERNOON_HOUR]
   end
 end
