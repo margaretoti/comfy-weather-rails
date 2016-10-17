1608dccd2ea6dcf091115815aac0bca189538488
sunshead <sunshead@gmail.com>
2016-10-17 12:11:46 -0400

    modify asset compiling for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module ComfyWeatherServer
     config.middleware.use Warden::Manager do |manager|
       manager.failure_app = lambda { |e| [ 401, {'Content-Type' => 'application/json'}, ['Authorization Failed'] ] }
     end
+    config.assets.initialize_on_precompile = false
   end
 end
